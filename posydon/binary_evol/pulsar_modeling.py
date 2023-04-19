603ce8c9fae01fea5020adbdabe97aa7ee9890ac
Camille <cliotine@u.northwestern.edu>
2023-04-19 15:11:25 -0500

    added Bfield evolution to detached step, added functionality for pulsar death line

--- posydon/binary_evol/pulsar_modeling.py
+++ posydon/binary_evol/pulsar_modeling.py
@@ -50,6 +50,8 @@ class Pulsar:
 
         self.spin = self.draw_NS_spin()          ## NS spin angular frequency [1/s]
         self.Bfield = self.draw_NS_Bfield()      ## NS magnetic field [G]
+
+        self.alive_state = self.is_alive()
  
         
     def draw_NS_spin(self):
@@ -88,12 +90,23 @@ class Pulsar:
         alpha = 45*const.a2rad          ## angle between the axis of rotation and magnetic axis [rad]
         mu_0 = 1                        ## permeability of free space [has value unity in cgs]
         B_min = 1e8                     ## minimum Bfield strength at which Bfield decay ceases [G]
+        tau_d = 3*1e9*const.secyer      ## B-field decay timescale = 3 Gyr [s]
 
         I = self.moment_inertia
         R = self.radius
+        B_i = self.Bfield
 
-        omega_f = np.sqrt(1/(8*np.pi*R**6*np.sin(alpha)**2/(3*mu_0*const.clight**3*I) * B_min**2*delta_t + 1/self.spin**2)) 
+        ## evolve the NS B-field
+        B_f = (B_i - B_min) * np.exp(-delta_t/tau_d) + B_min
+        self.Bfield = B_f
+
+        ## evolve the NS spin
+        A = 8*np.pi*R**6*np.sin(alpha)**2/(3*mu_0*const.clight**3*I)
+        omega_f = np.sqrt(1/( A*(B_min**2*delta_t - tau_d*B_min*(B_f - B_i) - tau_d/2*(B_f**2 - B_i**2)) + 1/self.spin**2)) 
         self.spin = omega_f
+
+        ## check if pulsar crossed the death line
+        self.alive_state = self.is_alive()
 
     
     def RLO_evolve(self, delta_t, T, delta_M):
@@ -128,6 +141,8 @@ class Pulsar:
         B_f = self.Bfield/(1 + delta_M/(1e-6*const.Msun)) * np.exp(-(T-delta_t)/tau_d)
         self.Bfield = B_f
 
+        ## check if pulsar crossed the death line
+        self.alive_state = self.is_alive()
 
 
     def CE_evolve(self):
@@ -138,21 +153,24 @@ class Pulsar:
         ----------
         ''' 
         delta_M = 0.1*const.Msun  ## assume amount of mass accreted during CE = 0.1 Msun
-
-        ## params needed for RLO evolve
-        delta_t = 0     ## assume CE phase is instantaneous
+    
+        ## params needed for RLO evolve, assume CE phase is instantaneous
+        delta_t = 0     
         T = 0           
 
         self.RLO_evolve(delta_t, T, delta_M)
 
 
-    def check_death_line(self):
+    def is_alive(self):
         '''
         Check if the pulsar has crossed the death line.
-
-        Parameters
-        ----------
         ''' 
+
+        P = 2*np.pi/self.spin     ## spin period of the pulsar [s]
+        death_line = 0.17e12
+
+        if (self.Bfield/P**2 < death_line): return False
+        else: return True
 
 
 
