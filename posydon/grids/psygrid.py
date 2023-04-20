c891d4828a8af8018d3185eed9d809d496b533c9
mkruckow <122798003+mkruckow@users.noreply.github.com>
2023-04-20 17:31:42 +0200

    Fix bug in adding too many boundary systems
    Fix bug in adding too many boundary systems

--- posydon/grids/psygrid.py
+++ posydon/grids/psygrid.py
@@ -2062,17 +2062,17 @@ def join_grids(input_paths, output_path,
             new_detected_initial_RLO = get_detected_initial_RLO(grid)
             say(new_detected_initial_RLO)
             for new_sys in new_detected_initial_RLO:
+                e = False
                 for sys in detected_initial_RLO:
                     # check whether there are double entries
                     if (abs(sys["star_1_mass"]-new_sys["star_1_mass"])<1.0e-5 and
                         abs(sys["star_2_mass"]-new_sys["star_2_mass"])<1.0e-5):
+                        e = True
                         # if so, replace old entry if the new one has a larger period
                         if sys["period_days"]<new_sys["period_days"]:
                             sys = new_sys.copy()
-                    else:
-                        # otherwise add new entry
-                        detected_initial_RLO.append(new_sys)
-                if len(detected_initial_RLO)<1:
+                # add non existing new entry
+                if not e:
                     detected_initial_RLO.append(new_sys)
             say(detected_initial_RLO)
     
