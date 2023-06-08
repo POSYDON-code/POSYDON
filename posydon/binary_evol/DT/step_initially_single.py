8527591edebf8459865971c22388e33e82cf4a16
kasdaglie <kasdaglie@ufl.edu>
2023-06-08 10:02:12 -0400

    IsolatedStep changes

--- posydon/binary_evol/DT/step_initially_single.py
+++ posydon/binary_evol/DT/step_initially_single.py
@@ -31,7 +31,7 @@ from posydon.utils.common_functions import (
 from posydon.binary_evol.flow_chart import (STAR_STATES_CC)
 import posydon.utils.constants as const
 from posydon.binary_evol.DT.step_detached import detached_step
-from posydon.binary_evol.DT.step_isolated import isolated_step
+from posydon.binary_evol.DT.step_isolated import IsolatedStep
 
 import warnings
 
@@ -53,7 +53,7 @@ LIST_ACCEPTABLE_STATES_FOR_POSTMS = STAR_STATES_H_RICH.copy()
 LIST_ACCEPTABLE_STATES_FOR_POSTHeMS = STAR_STATES_HE_RICH.copy()
 [LIST_ACCEPTABLE_STATES_FOR_POSTHeMS.remove(x) for x in LIST_ACCEPTABLE_STATES_FOR_HeMS]
 
-class InitiallySingleStep(isolated_step):
+class InitiallySingleStep(IsolatedStep):
     """
     Prepare a runaway star to do an an isolated_step)
     """
