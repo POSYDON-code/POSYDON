15fc4c87eb67abce105137f3f3e1b11d39be4f9a
ssbvr <sim.bavera@gmail.com>
2023-08-09 15:24:27 +0200

    allow to specify arbitrary magnitude key

--- posydon/visualization/interpolation.py
+++ posydon/visualization/interpolation.py
@@ -328,13 +328,15 @@ class EvaluateIFInterpolator:
 
         pass
 
-    def plot2D(self, slice_3D_var_range, PLOT_PROPERTIES):
+    def plot2D(self, key, slice_3D_var_range, PLOT_PROPERTIES):
 
+        k_ind = self.out_keys.index(key)
+        
         mass_ratio = self.test_grid.initial_values["star_2_mass"] / self.test_grid.initial_values["star_1_mass"]
 
         slice = (mass_ratio >= slice_3D_var_range[0]) & (mass_ratio <= slice_3D_var_range[1])
 
-        slice_errs = np.nanmean(self.errs["relative"], axis = 1)
+        slice_errs = self.errs["relative"].T[k_ind]
         slice_errs = [slice_errs[i] if in_slice and i in self.errs["valid_inds"][0] else np.nan for i, in_slice in enumerate(slice)]
 
         fig = self.test_grid.plot2D('star_1_mass', 'period_days', np.array(slice_errs),
