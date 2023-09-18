421f2aacdbd4077de4eb1b625c89df709bd020d1
ssbvr <sim.bavera@gmail.com>
2023-09-18 11:44:25 +0200

    increase lenght mt_history oneline dataframe

--- posydon/popsyn/binarypopulation.py
+++ posydon/popsyn/binarypopulation.py
@@ -65,8 +65,8 @@ ONELINE_MIN_ITEMSIZE = {'state_i': 30, 'state_f': 30,
                         'S1_SN_type': 5, 'S2_SN_type': 5,
                         'interp_class_HMS_HMS' : 15, 'interp_class_CO_HeMS' : 15,
                         'interp_class_CO_HMS_RLO' : 15, 'interp_class_CO_HeMS_RLO' : 15,
-                        'mt_history_HMS_HMS' : 30, 'mt_history_CO_HeMS' : 30,
-                        'mt_history_CO_HMS_RLO' : 30, 'mt_history_CO_HeMS_RLO' : 30,
+                        'mt_history_HMS_HMS' : 40, 'mt_history_CO_HeMS' : 40,
+                        'mt_history_CO_HMS_RLO' : 40, 'mt_history_CO_HeMS_RLO' : 40,
                         }
 
 # BinaryPopulation will enforce a constant metallicity accross all steps that
