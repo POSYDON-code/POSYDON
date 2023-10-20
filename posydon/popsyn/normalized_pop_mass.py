9e000ad4a505547dac162a18a366c60f73cfde9f
kasdaglie <kasdaglie@phys.ufl.edu>
2023-10-20 18:14:56 -0400

    Introducing changes

--- posydon/popsyn/normalized_pop_mass.py
+++ posydon/popsyn/normalized_pop_mass.py
@@ -52,7 +52,7 @@ def initial_total_underlying_mass(df=None, **kwargs):
     else:
         sel = df['event'] == 'ZAMS'
         initial_ZAMS_TOTAL_mass = sum(df['S1_mass'][sel]+df['S2_mass'][sel])
-
+    #Eirini's changes to the IMF to include the 
     def imf_part_1(m, m_min, alpha1):
         return (m/m_min)**-alpha1
 
@@ -72,7 +72,34 @@ def initial_total_underlying_mass(df=None, **kwargs):
             + ((1/(2-a3)) * (m_1/m_min)**-a1 * (m_2/m_1)**-a2
                * ((m_max**(2-a3) - m_2**(2-a3))/(m_2**-a3))))
         return mean_mass
+    #Eirini's suggestion is to built a universal mean_mass_of_binary that can be used both for model mass and the pop mass.
+    #Also if posydon goes to smaller masses this way will be flexible to that too.
+    def mean_mass_binary(f0, f_bin, m_1, m_2, m_min, m_max,
+                            alpha1, alpha2, alpha3):
+        if m_min <= 0.08:
+            #Include the all the imf parts
+            a1, a2, a3 = 1, 1, 1
 
+        elif 0.08 < m_min <= 0.5:
+            m1 = m_min
+            a1,a2,a3 = 0, 1, 1
+            alpha1 = 0
+        else: 
+            a1,a2,a3 = 0, 0, 1
+            alpha1,alpha2 = 0,0 
+            m_2 = m_min 
+        mean_mass = f0 * (1 + (f_bin/2))* (1 + (f_bin/2))*(quad(lambda x: imf_part_1(x, m_min, a1)*x , m_min, m_1)[0]
+        + quad(lambda x: imf_part_2(x,m_1,m_min,alpha1,alpha2)*x, m_1, m_2)[0]
+        + quad(lambda x: imf_part_3(x, m_1, m_2, m_min, alpha1, alpha2, alpha3)*x, m_2, m_max)[0])
+        
+        return mean_mass   
+    #Normalization parameter.
+    def f_0(a1,a2,a3,alpha1,alpha2,alpha3):
+        f0 = 1/(quad(a1*imf_part_1, m_min, m_1, args=(m_min, alpha1))[0]
+            + quad(a2*imf_part_2, m_1, m_2, args=(m_1, m_min, alpha1, alpha2))[0]
+            + quad(a3*imf_part_3, m_2, m_max, args=(m_1, m_2, m_min, alpha1,alpha2, alpha3))[0])
+        return f0
+    
     def mean_mass_simulated(alpha3, m_a, m_b):
         a = alpha3
         mean_mass_sim = (3/2) * ((1-a) / (2-a)) * (
@@ -106,6 +133,7 @@ def initial_total_underlying_mass(df=None, **kwargs):
     else:
         raise ValueError("Scheme not included yet")
 
+    f_bin_pop = kwargs['binary_fraction_const']
     f_bin = 0.7
     m_min = 0.01
     m_max = 200.0
