bdd53ae48881a9e4acd7e1e364f98e094f1759a2
Max <14039563+maxbriel@users.noreply.github.com>
2023-10-26 16:33:32 +0200

    Concatenate fix for casting current scalar_names (#167)
    * add casting of scalar_names to float64 when writing to dataframe
    
    * remove cast in singlestar and create standard scalar_names casting in io.py
    
    * add scalar_names type casting
    
    * add missed typo

--- posydon/popsyn/io.py
+++ posydon/popsyn/io.py
@@ -134,6 +134,15 @@ EXTRA_BINARY_COLUMNS_DTYPES = {
 # no default extras for history attributes
 EXTRA_STAR_COLUMNS_DTYPES = {}
 
+SCALAR_NAMES_DTYPES = {
+    'natal_kick_array_0': 'float64',
+    'natal_kick_array_1': 'float64',
+    'natal_kick_array_2': 'float64',
+    'natal_kick_array_3': 'float64',
+    'SN_type':'string',
+    'f_fb': 'float64',
+    'spin_orbit_tilt': 'float64',
+}
 
 
 def clean_binary_history_df(binary_df, extra_binary_dtypes_user=None, 
@@ -264,10 +273,11 @@ def clean_binary_oneline_df(oneline_df, extra_binary_dtypes_user=None,
                            **extra_binary_dtypes_user}
     # combine columns for star 1 and star 2 history with extras
     SP_comb_S1_dict = {**STARPROPERTIES_DTYPES, **EXTRA_STAR_COLUMNS_DTYPES, 
-                       **extra_S1_dtypes_user}
+                       **SCALAR_NAMES_DTYPES, **extra_S1_dtypes_user}
     SP_comb_S2_dict = {**STARPROPERTIES_DTYPES, **EXTRA_STAR_COLUMNS_DTYPES, 
-                       **extra_S2_dtypes_user}
+                       **SCALAR_NAMES_DTYPES, **extra_S2_dtypes_user}
     
+
     # All default & user passed keys which we have mappings for
     binary_keys = set( [key + '_i' for key in BP_comb_extras_dict.keys()] 
                      + [key + '_f' for key in BP_comb_extras_dict.keys()] )
@@ -275,17 +285,26 @@ def clean_binary_oneline_df(oneline_df, extra_binary_dtypes_user=None,
                  + ['S1_' + key + '_f' for key in SP_comb_S1_dict.keys()] )
     S2_keys = set( ['S2_' + key + '_i' for key in SP_comb_S2_dict.keys()]
                  + ['S2_' + key + '_f' for key in SP_comb_S2_dict.keys()] )
-    
+    scalar_keys = set( ['S1_' + key for key in SCALAR_NAMES_DTYPES.keys()]
+                     + ['S2_' + key for key in SCALAR_NAMES_DTYPES.keys()] )
+                        
     # Find common keys between the binary_df and our column-dtype mapping
-    common_keys =  oneline_columns & ( binary_keys | S1_keys | S2_keys )
+    common_keys =  oneline_columns & ( binary_keys 
+                                     | S1_keys
+                                     | S2_keys 
+                                     | scalar_keys)
     
 
     # Create a dict with column-dtype mapping only for columns in binary_df
     common_dtype_dict = {}
     # helper function to remove the '_i' and '_f' 
-    strip_prefix_and_suffix = lambda key : key.strip('_i').strip('_f').strip('S1_').strip('S2_')
+    strip_prefix_and_suffix = lambda key : key.replace('_i', '').replace('_f', '') \
+                                             .replace('S1_', '').replace('S2_', '')
     for key in common_keys:
-        if key in binary_keys:
+        if key in scalar_keys:
+            common_dtype_dict[key] = SCALAR_NAMES_DTYPES.get(
+                                        key.replace('S1_', '').replace('S2_', '') )
+        elif key in binary_keys:
             common_dtype_dict[key] = BP_comb_extras_dict.get( strip_prefix_and_suffix(key) )
         elif key in S1_keys:
             common_dtype_dict[key] = SP_comb_S1_dict.get( strip_prefix_and_suffix(key) )
