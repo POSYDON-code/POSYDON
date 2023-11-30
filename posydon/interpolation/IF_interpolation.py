Fix a bug in normalization
normalized = X ->
normalized = X.copy()
otherwise the input array got normalized unexpectedly

posydon/interpolation/IF_interpolation.py
@@ -1420,7 +1420,7 @@ class Scaler:
         
         else:
 
-            normalized = X
+            normalized = X.copy()
 
             classes = np.unique(klass)
 