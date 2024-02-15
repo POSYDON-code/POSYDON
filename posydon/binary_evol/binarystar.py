b52d19d435f17a14c993eaba66c585ff80bbcdb0
ezapartas <ezapartas@gmail.com>
2024-02-15 17:51:57 +0200

    Inhomogeneous binary array (#251)
    * trying to solve both inhomogeneity of singlestar array and solve the specific issue was occurring for track_interpolation and intial_RLO
    
    * I uncommented all changes (if someone wants to test ontly the specific track_interpolation error, they should comment the single/binarystar fix)
    
    * erased the homogeneous_array function in common functions
    
    * add a call to the interpolator before appending information to the BinaryStar to check what the interaction is
    
    * replaced NAN values to np.nan
    
    ---------
    
    Co-authored-by: Max Briel <max.briel@gmail.com>
    Co-authored-by: dimsour94 <dimitris.souropanis@gmail.com>

--- posydon/binary_evol/binarystar.py
+++ posydon/binary_evol/binarystar.py
@@ -164,7 +164,7 @@ class BinaryStar:
             self.mass_transfer_case = 'None'
         # if not hasattr(self, 'V_sys'):
         #     self.V_sys = [0, 0, 0]
-        
+
         # store interpolation_class and mt_history for each step_MESA
         for grid_type in ['HMS_HMS','CO_HMS_RLO','CO_HeMS','CO_HeMS_RLO']:
             if not hasattr(self, f'interp_class_{grid_type}'):
@@ -213,7 +213,7 @@ class BinaryStar:
             total_state = (self.star_1.state, self.star_2.state, self.state,
                            self.event)
             next_step_name = self.properties.flow.get(total_state)
-            
+
             if next_step_name is None:
                 warnings.warn("Undefined next step given stars/binary states "
                               "{}.".format(total_state))
@@ -330,7 +330,7 @@ class BinaryStar:
         extra_binary_cols_dict = kwargs.get('extra_columns', {})
         extra_columns = list(extra_binary_cols_dict.keys())
         extra_columns_dtypes_user = list(extra_binary_cols_dict.values())
-        
+
         all_keys = (["binary_index"]
                     + [key+'_history' for key in BINARYPROPERTIES]
                     + extra_columns)
@@ -345,6 +345,7 @@ class BinaryStar:
                             + [key+'_history' for key in user_keys_to_save]
                             + extra_columns)
 
+
         try:
             data_to_save = [getattr(self, key) for key in keys_to_save[1:]]
             col_lengths = [len(x) for x in data_to_save]
@@ -353,16 +354,16 @@ class BinaryStar:
             # binary_index
             data_to_save.insert(0, [self.index]*max_col_length)
 
-            where_none = np.array([[True if var is None else False
-                                    for var in column]
-                                   for column in data_to_save], dtype=bool)
-
             # If a binary fails, usually history cols have diff lengths.
             # This should append NAN to create even columns.
             all_equal_length_cols = len(set(col_lengths)) == 1
             if not all_equal_length_cols:
                 for col in data_to_save:
-                    col.extend(['NAN'] * abs(max_col_length - len(col)))
+                    col.extend([np.nan] * abs(max_col_length - len(col)))
+
+            where_none = np.array([[True if var is None else False
+                                    for var in column]
+                                   for column in data_to_save], dtype=bool)
 
         except AttributeError as err:
             raise AttributeError(
@@ -589,7 +590,7 @@ class BinaryStar:
             oneline_df['WARNING'] = [0]
 
         oneline_df.set_index('binary_index', inplace=True)
-        
+
         # try to coerce data types automatically
         oneline_df = oneline_df.infer_objects()
 
@@ -602,7 +603,7 @@ class BinaryStar:
                                             extra_binary_dtypes_user=extra_binary_cols_dict,
                                             extra_S1_dtypes_user=extra_s1_cols_dict,
                                             extra_S2_dtypes_user=extra_s2_cols_dict)
-        
+
         return oneline_df
 
     @classmethod
