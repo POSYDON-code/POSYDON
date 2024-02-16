e233793c8b9397eb15c90b39dd432ea9953cb65a
Camille Liotine <cliotine@u.northwestern.edu>
2024-02-16 10:55:43 -0600

    add GridError and FlowError to POSYDON_Exception

--- posydon/utils/posydon_exception.py
+++ posydon/utils/posydon_exception.py
@@ -1,8 +1,21 @@
 class POSYDON_Exception(Exception):
     """
-
+    POSYDON Exception class that includes subclasses for POSYDON-specific errors.
+    
     """
-    def __init__(self, message):
+    def __init__(self, error_checking=False, message=""):
+        self.error_checking = error_checking
         self.message = message
-        super().__init__(self.message)  
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+    
+class GridError(POSYDON_Exception):    
+    def __init__(self, err_message):
+        super().__init__(message=err_message)
+
+class FlowError(POSYDON_Exception):    
+    def __init__(self, err_message):
+        super().__init__(message=err_message)
 
