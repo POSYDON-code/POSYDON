9c1527246b17f0a82320f0b2d51ca006d52d63da
Chase Kimball <charleskimball2012@u.northwestern.edu>
2024-02-22 09:07:12 -0600

    Tilt tracking (#129)
    * Added tilt tracking
    
    * variable names and pulling first_SN outside of the flag_binary conditional
    
    * Added tilt tracking
    
    * variable names and pulling first_SN outside of the flag_binary conditional
    
    * added rotate() to common_functions.py
    
    * added flag for if first SN already occurred
    
    * allow tilt to be positive or negative according to direction of Vkz

--- posydon/binary_evol/binarystar.py
+++ posydon/binary_evol/binarystar.py
@@ -162,6 +162,13 @@ class BinaryStar:
             self.inspiral_time = None
         if not hasattr(self, 'mass_transfer_case'):
             self.mass_transfer_case = 'None'
+
+        if not hasattr(self, 'true_anomaly_first_SN'):
+            self.true_anomaly_SN1 = None
+        if not hasattr(self, 'true_anomaly_second_SN'):
+            self.true_anomaly_SN2 = None
+        if not hasattr(self, 'first_SN_already_occurred'):
+            self.first_SN_already_occurred = False
         # if not hasattr(self, 'V_sys'):
         #     self.V_sys = [0, 0, 0]
 
