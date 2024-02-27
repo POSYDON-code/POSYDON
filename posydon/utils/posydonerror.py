394eb15c8a07ca197c6fc479c9e423d3a7e78fa6
kkovlakas <kkovla@gmail.com>
2024-02-27 17:27:44 +0100

    Enhancing the POSYDONError to accept objects and print them if they are stars.

--- posydon/utils/posydonerror.py
+++ posydon/utils/posydonerror.py
@@ -1,13 +1,33 @@
+from posydon.binary_evol.binarystar import BinaryStar
+from posydon.binary_evol.singlestar import SingleStar
+
 class POSYDONError(Exception):
     """
     POSYDON Exception class that includes subclasses for POSYDON-specific errors.
 
     """
-    def __init__(self,message=""):
+    def __init__(self, message="", objects=None):
+        """General POSYDON exception.
+
+        Parameters
+        ----------
+        message : str
+            POSYDONError message.
+        objects : None or list of objects.
+            If not None, a list of accompanied objects.
+
+        """
         self.message = message
+        self.objects = objects
         super().__init__(self.message)
 
     def __str__(self):
+        result = self.message
+        if self.objects is not None:
+            for i, object in enumerate(self.objects):
+                if isinstance(object, (BinaryStar, SingleStar)):
+                    result += (f"\nOBJECT #{i+1} ({type(object)})\n"
+                               + str(object))
         return self.message
 
 class GridError(POSYDONError):
