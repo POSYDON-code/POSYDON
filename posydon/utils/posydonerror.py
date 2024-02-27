ccb37e1cdd83cbcff3d4905b7539b9bd06f3f60a
kkovlakas <kkovla@gmail.com>
2024-02-27 16:38:53 +0100

    Removing undefined class property.

--- posydon/utils/posydonerror.py
+++ posydon/utils/posydonerror.py
@@ -1,21 +1,19 @@
 class POSYDONError(Exception):
     """
     POSYDON Exception class that includes subclasses for POSYDON-specific errors.
-    
+
     """
     def __init__(self,message=""):
-        self.error_checking = error_checking
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
         return self.message
-    
-class GridError(POSYDONError):    
+
+class GridError(POSYDONError):
     def __init__(self, err_message):
         super().__init__(message=err_message)
 
-class FlowError(POSYDONError):    
+class FlowError(POSYDONError):
     def __init__(self, err_message):
         super().__init__(message=err_message)
-
