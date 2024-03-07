3df2f49e44c4ab57487b4372984f341e715e88ce
Max Briel <max.briel@gmail.com>
2024-03-07 10:34:32 +0100

    add correct GRB_selection function

--- posydon/popsyn/transient_select_funcs.py
+++ posydon/popsyn/transient_select_funcs.py
@@ -7,265 +7,116 @@ from tqdm import tqdm
 
 PATH_TO_PDET_GRID = os.path.join(PATH_TO_POSYDON_DATA, 'POSYDON_data/selection_effects/pdet_grid.hdf5')
 
-# RAW FUNCTIONS! NEED TO BE EDITED TO WORKS WITH THE NEW STRUCTURE!!!!
+def GRB_selection(history_chunk, oneline_chunk, formation_channels_chunk=None, S1_S2='S1',):
+    """A GRB selection function to create a transient population of LGRBs.
+    
+    This function requires a wrapper function to be used to create a transient population, because
+    of the extra parameters that are needed to be passed to the function (S1_S2).
+    
+    >>> def GRB_selection_wrapper(history_chunk, oneline_chunk, formation_channels_chunk=None):
+    ...     return GRB_selection(history_chunk, oneline_chunk, formation_channels_chunk, S1_S2='S1')
+        
+    This is an example function for selecting LGRBs and some of their properties.
+    Additional properties from the history or oneline can be added to the transient population.
+    The output dataframe contains the following columns:
+    - time : the time of the event
+    - metallicity : the metallicity of the event
+    - S1_m_disk_radiated : the mass of the disk radiated by the first star
+    - S2_m_disk_radiated : the mass of the disk radiated by the second star
+    
+    Columns with pre and post SN data:
+    - S1_mass : the mass of the first star
+    - S2_mass : the mass of the second star
+    - S1_spin : the spin of the first star
+    - S2_spin : the spin of the second star
+    - orbital_period : the orbital period of the binary
+    - eccentricity : the eccentricity of the binary
 
-def create_GRB_population(output_file=None, GRB_properties={}, additional_oneline_cols=None):
-    '''Create a GRB population from the parsed population.
-    
-    The Synthetic Population will contain a 'time for each 'event',
-    which represents the time after starburst that the event takes place.
-    
     Parameters
     ----------
-    GRB_properties : dict
-        Dictionary containing the GRB properties to use.
-        The dictionary must contain
-        
-        - 'GRB_efficiency',
-        - 'GRB_beaming',
-        - 'E_GRB_iso_min'
+    history_chunk : pd.DataFrame
+        The history chunk of the population.
+    oneline_chunk : pd.DataFrame
+        The oneline chunk of the population.
+    formation_channels_chunk : pd.DataFrame (can be made optional)
+        The formation pathway chunk of the population.
+    S1_S2 : str
+        The star that will be the progenitor of the GRB. Can be either 'S1' or 'S2'.
     
-    '''
-    # The user will have done an initial parse, when inputting the parsed
-    # population data. Additional filtering can be done manually.
-    # Using the data from the parsed population, 
-    # we find the DCO at their formation.
-    if output_file is None:
-        GRB_synthetic_population = SyntheticPopulation(self.parsed_pop_file,
-                                                        verbose=self.verbose)
-        parsed_store = pd.HDFStore(self.parsed_pop_file, mode='a')
-        output_store = parsed_store
-    else:
-        GRB_synthetic_population = SyntheticPopulation(output_file,
-                                                        verbose=self.verbose)
-        
-        # write population data to the new file!
-        GRB_synthetic_population.io.save_ini_params(self)
-        GRB_synthetic_population.io.save_parse_kwargs(self)
-        GRB_synthetic_population.io.save_per_metallicity_info(self)
-        GRB_synthetic_population.io.save_filename(self)
-        GRB_synthetic_population.io.save_parsed_pop_path(self.parsed_pop_file)
-        
-        # load data into GRB_data
-        GRB_synthetic_population.io.load_ini_params(GRB_synthetic_population)
-        GRB_synthetic_population.io.load_parse_kwargs(GRB_synthetic_population)
-        GRB_synthetic_population.io.load_per_metallicity_info(GRB_synthetic_population)
-        GRB_synthetic_population.io.load_filename(GRB_synthetic_population)
-        GRB_synthetic_population.io.load_parsed_pop_path(GRB_synthetic_population)
-        
-        # Open the file for writing the synthetic population
-        parsed_store = pd.HDFStore(self.parsed_pop_file, mode='r')
-        output_store = pd.HDFStore(GRB_synthetic_population.pop_file, mode='a')
-
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing the transient population of LGRBs with the columns mentioned above.
     
-    if '/synthetic' in output_store.keys():
-        print('A synthetic population already exists in this file.\
-            The current population will be removed!')      
-        del output_store['synthetic']
+    Example
+    -------
     
-    GRB_synthetic_population.population_selection = 'GRB'
-    GRB_synthetic_population._save_population_selection()
-    GRB_synthetic_population.model_parameters = GRB_properties  
+    >>> GRB_selection_wrapper(pop.history[0], pop.oneline[0], pop.formation_channels.loc[0])
+    >>> pop.create_transient_population(GRB_selection_wrapper, name='GRB_S1')
+    """
     
-    if ('GRB_efficiency' not in GRB_properties or
-        GRB_properties['GRB_efficiency'] is None):
-        raise ValueError('Missing GRB_efficiency variable in the MODEL!')
-    if ('GRB_beaming' not in GRB_properties or
-        GRB_properties['GRB_beaming'] is None):
-        raise ValueError('Missing GRB_beaming variable in the MODEL!')
-    if ('E_GRB_iso_min' not in GRB_properties or
-        GRB_properties['E_GRB_iso_min'] is None):
-        raise ValueError('Missing GRB_beaming variable in the MODEL!')
-    
-    # S1 and S2 mass are autmatically added to their respective columns
-    # changing columns over the SN
-    columns_pre_post = ['orbital_period', 'eccentricity']
-    # unchanged columns
+   
+    columns_pre_post  = ['orbital_period', 'eccentricity', 'S1_spin', 'S2_spin']
     columns = ['metallicity']
-    # LGRB parameters
-    oneline_columns = ['S1_spin_orbit_tilt']
-    if additional_oneline_cols is not None:
-        for c in additional_oneline_cols:
-            if c not in oneline_columns:
-                oneline_columns.append(c)
-                
-                
-    min_itemsize = {'channel': 100,}
-    min_itemsize.update({key:val for key, val in 
-                            HISTORY_MIN_ITEMSIZE.items() 
-                            if key in columns_pre_post})
-    min_itemsize.update({key:val for key, val in
-                            HISTORY_MIN_ITEMSIZE.items()
-                            if key in columns})
-    min_itemsize.update({key:val for key, val in
-                            ONELINE_MIN_ITEMSIZE.items()
-                        if key in oneline_columns})
+    oneline_columns = ['S1_m_disk_radiated', 'S2_m_disk_radiated']
+   
+    if S1_S2 == 'S1':
+        indices_selection = oneline_chunk.index[oneline_chunk['S1_m_disk_radiated'] > 0.0]
+        oneline_chunk = oneline_chunk.drop(columns=['S2_m_disk_radiated'])
+    elif S1_S2 == 'S2':
+        indices_selection = oneline_chunk.index[oneline_chunk['S2_m_disk_radiated'] > 0.0]
+        oneline_chunk = oneline_chunk.drop(columns=['S1_m_disk_radiated'])
+    else:
+        raise ValueError('S1_S2 must be either S1 or S2')
     
-    def multi_columns_read(key, parsed_store, columns, previous, end, additional_oneline_cols=None):
-        '''Read multiple columns from the history dataframe
-        '''
-        df = pd.DataFrame()
-        for c in columns:
-            df[c] = parsed_store.select_column(key, c, start=previous, stop=end)
-        df.index = parsed_store.select_column(key, 'index', start=previous, stop=end)
-        return df                  
+    # no events in this chunk
+    if len(indices_selection) == 0:
+        return None
+    # filter out the events that are not relevant for the LGRB formation
+    selection = history_chunk.loc[indices_selection]    
+    if S1_S2 == 'S1':
+        S_mask = (selection['S1_state'] == 'BH') & (selection['S1_state'] != 'BH').shift(1) & (selection['step_names'] == 'step_SN')
+    elif S1_S2 == 'S2':
+        S_mask = (selection['S2_state'] == 'BH') & (selection['S2_state'] != 'BH').shift(1) & (selection['step_names'] == 'step_SN')
     
-    def GRB_data_store(tmp_df, parsed_store, previous, end, S1_S2='S1'):                    
-        indices = tmp_df.index
-        
-        # Read history of the chunk
-        hist = multi_columns_read('history',
-                                parsed_store, 
-                                ['S1_state', 'S2_state', 'event', 'step_names', 'time', 'S1_mass', 'S2_mass', 'metallicity', 'orbital_period', 'eccentricity'],
-                                previous,
-                                end)
-        
-        GRB_df_transients = pd.DataFrame()
-        
-        if S1_S2 == 'S1':
-        # get the SN event of the GRB
-        # This also select the second step_SN if S1_S2 goes SN first.
-            logic1 = self.apply_logic(hist,
-                                    S1_state='BH',
-                                    S2_state=None,
-                                    step_name='step_SN',
-                                    invert_S1S2=False)
-        elif S1_S2 == 'S2':
-            logic1 = self.apply_logic(hist,
-                                    S1_state=None,
-                                    S2_state='BH',
-                                    step_name='step_SN',
-                                    invert_S1S2=False)   
+    GRB_df_synthetic = pd.DataFrame(index=indices_selection)
+    # Pre and post SN data
+    post_SN_hist = selection[S_mask]
+    pre_mask = S_mask.shift(-1)
+    pre_mask.iloc[-1] = False
+    pre_SN_hist = selection[pre_mask]
     
-        # select the previous row
-        mask2 = logic1.shift(-1)
-        mask2.iloc[-1]  = False
+    if S1_S2 == 'S1':
+        columns_pre_post.append('S1_mass')
+        columns.append('S2_mass')
+    elif S1_S2 == 'S2':
+        columns_pre_post.append('S2_mass')
+        columns.append('S1_mass')
         
-        # Select just the event where S1_S2 was not BH before undergoing the SN
-        S1_SN = hist.loc[mask2].loc[indices][f'{S1_S2}_state'] != 'BH'
-        post_sn = hist.loc[logic1].loc[indices][S1_SN]
-        pre_sn = hist.loc[mask2].loc[indices][S1_SN]
-        # write properties to the Synthetic Population
-        GRB_df_transients = pd.DataFrame()
-        if S1_S2 == 'S1':
-            columns_pre_post.append('S1_mass')
-            columns.append('S2_mass')
-        elif S1_S2 == 'S2':
-            columns_pre_post.append('S2_mass')
-            columns.append('S1_mass')
-            
-        for c in columns_pre_post:
-            GRB_df_transients['preSN_'+c] = pre_sn[c].valuesS
-            
-            GRB_df_transients['postSN_'+c] = post_sn[c].values
+    
+    for col in columns_pre_post:
+        GRB_df_synthetic[col+'_preSN'] = pre_SN_hist[col].values
+        GRB_df_synthetic[col+'_postSN'] = post_SN_hist[col].values
         
-        GRB_df_transients.index = post_sn.index
-        GRB_df_transients['time'] = post_sn['time'].values * 1e-6 # Myr
-        for c in columns:
-            GRB_df_transients[c] = pre_sn[c].values
-        
-        # add oneline parameters
-        df_oneline = multi_columns_read('oneline',
-                                        parsed_store,
-                                        oneline_columns,
-                                        i,
-                                        i+self.chunksize)
-        
-        for c in oneline_columns:
-            GRB_df_transients[c] = df_oneline.loc[indices,c].values
-        
-        # Add GRB parameters
-        for c in tmp_df.columns:
-            GRB_df_transients[c] = tmp_df.loc[indices,c].values
+    # unchanged columns
+    for col in columns:
+        GRB_df_synthetic[col] = post_SN_hist[col].values    
 
+    # oneline data
+    for col in oneline_chunk.columns:
+        GRB_df_synthetic[col] = oneline_chunk.loc[indices_selection][col].values
         
+    if any(formation_channels_chunk != None):
+        formation_channels_chunk = formation_channels_chunk.loc[indices_selection]
+        if S1_S2 == 'S1':
+            GRB_df_synthetic['channel'] = formation_channels_chunk['channel'].str.split('_CC1').str[0].apply(lambda x: x+'_CC1')
+        elif S1_S2 == 'S2':
+            GRB_df_synthetic['channel'] = formation_channels_chunk['channel'].str.split('_CC2').str[0].apply(lambda x: x+'_CC2')
         
-        # add formation channels
-        df_channel = parsed_store.select_column('formation_channels',
-                                                'channel', 
-                                                start = i,
-                                                stop=i+self.chunksize)
+    # calculate the time!
+    GRB_df_synthetic['time'] = post_SN_hist['time'].values * 1e-6 # convert to Myr
         
-        df_channel.index = parsed_store.select_column('formation_channels',
-                                                        'index',
-                                                        start = i,
-                                                        stop=i+self.chunksize)
-        
-        GRB_df_transients['channel'] = df_channel.loc[indices].values
-        
-        return GRB_df_transients
-    
-    history_events = parsed_store.select_column('history', 'index')
-    history_lengths = history_events.groupby(history_events).count()
-    del history_events
-    
-    unique_binary_indices = self.indices
-        
-    if self.verbose: print('looping over the binaries now')
-    
-    previous = 0
-    for i in tqdm(range(0,len(unique_binary_indices), self.chunksize), disable=not self.verbose):
-        
-        selection = unique_binary_indices[i:i+self.chunksize]
-        end = previous + history_lengths[i:i+self.chunksize].sum()
-                    
-        # Read oneline
-        m_disk_radiated = parsed_store.select_column('oneline',
-                                                        'S1_m_disk_radiated',
-                                                        start = i,
-                                                        stop=i+self.chunksize)
-        m_disk_radiated = pd.concat([m_disk_radiated, 
-                                        parsed_store.select_column('oneline',
-                                                            'S2_m_disk_radiated',
-                                                            start = i,
-                                                            stop=i+self.chunksize)],
-                                axis=1)
-        
-        m_disk_radiated.index = parsed_store.select_column('oneline',
-                                                            'index',
-                                                            start = i,
-                                                            stop=i+self.chunksize)
-        
-        tmp_df = get_GRB_properties(m_disk_radiated,
-                                    GRB_properties['GRB_efficiency'],
-                                    GRB_properties['GRB_beaming'],
-                                    GRB_properties['E_GRB_iso_min']
-                                    )
-        
-        # S1 GRBs
-        S1_tmp_df = tmp_df[tmp_df['GRB1'] == True]
-        S1_GRB_df_transients = GRB_data_store(S1_tmp_df, parsed_store, previous, end, 'S1')
-        # Set all S2 columns to NaN
-        for c in S1_GRB_df_transients.columns:
-            if c in ['S2_eta', 'S2_E_GRB', 'S2_f_beaming', 'S2_E_GRB_iso', 'S2_L_GRB_iso', 'GRB2']:
-                S1_GRB_df_transients[c] = None
-        
-        
-        # S2 GRBs
-        S2_tmp_df = tmp_df[tmp_df['GRB2'] == True]
-        S2_GRB_df_transients = GRB_data_store(S2_tmp_df, parsed_store, previous, end, 'S2')
-        # Set all S1 columns to NaN
-        for c in S2_GRB_df_transients.columns:
-            if c in ['S1_eta', 'S1_E_GRB', 'S1_f_beaming', 'S1_E_GRB_iso', 'S1_L_GRB_iso', 'GRB1']:
-                S2_GRB_df_transients[c] = None
-        
-        out = pd.concat([S1_GRB_df_transients, S2_GRB_df_transients])
-        
-        out['GRB1'] = out['GRB1'].astype(bool)
-        out['GRB2'] = out['GRB2'].astype(bool)
-        # store the synthetic populations
-        output_store.append('synthetic',
-                            out,
-                            format='table',
-                            data_columns=True,
-                            min_itemsize=min_itemsize
-                            )
-        previous = end
-        
-        
-    output_store.close()
-    parsed_store.close()
-    return GRB_synthetic_population
+    return GRB_df_synthetic
 
 
 def chi_eff(m_1, m_2, a_1, a_2, tilt_1, tilt_2):
