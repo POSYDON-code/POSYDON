57e93ce5fe3abf82698de01d1dd3f2be239f956b
Max Briel <max.briel@gmail.com>
2024-03-07 07:50:51 +0100

    rate_calculation cleanup

--- posydon/popsyn/rate_calculation.py
+++ posydon/popsyn/rate_calculation.py
@@ -1,6 +1,7 @@
-
-__author__ = ['Simone Bavera <Simone.Bavera@unige.ch>',
-              'Max Briel <max.briel@unige.ch>',]
+__author__ = [
+    "Simone Bavera <Simone.Bavera@unige.ch>",
+    "Max Briel <max.briel@unige.ch>",
+]
 
 from posydon.utils.constants import Zsun
 
@@ -14,21 +15,21 @@ from astropy import units as u
 
 
 DEFAULT_MODEL = {
-    'delta_t' : 100, # Myr
-    'SFR' : 'IllustrisTNG',
-    'sigma_SFR' : None,
-    'Z_max' : 5.,
-    'select_one_met' : False,
-    'dlogZ' : None, # e.g, [np.log10(0.0142/2),np.log10(0.0142*2)]
-    'Zsun' : Zsun,
-    'compute_GRB_properties' : False,
-    'GRB_beaming' : 1., # e.g., 0.5, 'Goldstein+15'
-    'GRB_efficiency' : 0., # e.g., 0.01
-    'E_GRB_iso_min' : 0., # e.g., 1e51 erg 
+    "delta_t": 100,  # Myr
+    "SFR": "IllustrisTNG",
+    "sigma_SFR": None,
+    "Z_max": 5.0,
+    "select_one_met": False,
+    "dlogZ": None,  # e.g, [np.log10(0.0142/2),np.log10(0.0142*2)]
+    "Zsun": Zsun,
+    "compute_GRB_properties": False,
+    "GRB_beaming": 1.0,  # e.g., 0.5, 'Goldstein+15'
+    "GRB_efficiency": 0.0,  # e.g., 0.01
+    "E_GRB_iso_min": 0.0,  # e.g., 1e51 erg
 }
 
 
-def get_shell_comoving_volume(z_hor_i, z_hor_f, sensitivity='infinite'):
+def get_shell_comoving_volume(z_hor_i, z_hor_f, sensitivity="infinite"):
     """Compute comoving volume corresponding to a redshift shell.
 
     Parameters
@@ -48,21 +49,31 @@ def get_shell_comoving_volume(z_hor_i, z_hor_f, sensitivity='infinite'):
         and z_hor_f in Gpc^3.
 
     """
-    c = const.c.to('Gpc/yr').value  # Gpc/yr
-    H_0 = cosmology.H(0).to('1/yr').value # km/Gpc*s
+    c = const.c.to("Gpc/yr").value  # Gpc/yr
+    H_0 = cosmology.H(0).to("1/yr").value  # km/Gpc*s
+
     def E(z):
         Omega_m = cosmology.Om0
-        Omega_L = 1-cosmology.Om0
-        return np.sqrt(Omega_m*(1.+z)**3+Omega_L)
-    def f(z,sensitivity):
-        if sensitivity=='infinite':
-            return (1./(1.+z) * 4*np.pi*c / H_0
-                    * (get_comoving_distance_from_redshift(z)
-                    * 10**(-3.))**2. / E(z))
+        Omega_L = 1 - cosmology.Om0
+        return np.sqrt(Omega_m * (1.0 + z) ** 3 + Omega_L)
+
+    def f(z, sensitivity):
+        if sensitivity == "infinite":
+            return (
+                1.0
+                / (1.0 + z)
+                * 4
+                * np.pi
+                * c
+                / H_0
+                * (get_comoving_distance_from_redshift(z) * 10 ** (-3.0)) ** 2.0
+                / E(z)
+            )
         else:
             # TODO: peanut-shaped antenna patter comoving volume calculation
-            raise ValueError('Sensitivity not supported!')
-    return sp.integrate.quad(f, z_hor_i, z_hor_f, args=(sensitivity))[0] # Gpc^3
+            raise ValueError("Sensitivity not supported!")
+
+    return sp.integrate.quad(f, z_hor_i, z_hor_f, args=(sensitivity))[0]  # Gpc^3
 
 
 def get_comoving_distance_from_redshift(z):
@@ -98,6 +109,7 @@ def get_cosmic_time_from_redshift(z):
     """
     return cosmology.age(z).value  # Gyr
 
+
 def redshift_from_cosmic_time_interpolator():
     """Interpolator to compute the cosmological redshift given the cosmic time.
 
@@ -109,11 +121,11 @@ def redshift_from_cosmic_time_interpolator():
     """
     # astropy z_at_value method is too slow to compute z_mergers efficinty
     # we must implement interpolation
-    t = np.linspace(1e-2, cosmology.age(1e-08).value*0.9999999, 1000)
+    t = np.linspace(1e-2, cosmology.age(1e-08).value * 0.9999999, 1000)
     z = np.zeros(1000)
     for i in range(1000):
         z[i] = z_at_value(cosmology.age, t[i] * u.Gyr)
-    f_z_m = interp1d(t, z, kind='cubic')
+    f_z_m = interp1d(t, z, kind="cubic")
     return f_z_m
 
 
@@ -153,45 +165,46 @@ def get_redshift_bin_edges(delta_t):
         Redshift bin edges.
 
     """
-    n_redshift_bin_centers = int(cosmology.age(0).to('Myr').value/delta_t)
+    n_redshift_bin_centers = int(cosmology.age(0).to("Myr").value / delta_t)
     # generate t_birth at the middle of each self.delta_t bin
-    t_birth_bin = [cosmology.age(0.).value]
-    for i in range(n_redshift_bin_centers+1):
-        t_birth_bin.append(t_birth_bin[i] - delta_t*1e-3) # Gyr
+    t_birth_bin = [cosmology.age(0.0).value]
+    for i in range(n_redshift_bin_centers + 1):
+        t_birth_bin.append(t_birth_bin[i] - delta_t * 1e-3)  # Gyr
     # compute the redshift
     z_birth_bin = []
     for i in range(n_redshift_bin_centers):
         # do not count first edge, we add z=0. later
-        z_birth_bin.append(z_at_value(cosmology.age, t_birth_bin[i+1] * u.Gyr))
+        z_birth_bin.append(z_at_value(cosmology.age, t_birth_bin[i + 1] * u.Gyr))
     # add the first and last bin edge at z=0. and z=inf.=100
-    z_birth_bin = np.array([0.]+z_birth_bin+[100.])
+    z_birth_bin = np.array([0.0] + z_birth_bin + [100.0])
 
     return z_birth_bin
-   
+
+
 def get_redshift_bin_centers(delta_t):
     """Compute the redshift bin centers.
-    
+
     Parameters
     ----------
     delta_t : double
         Time interval in Myr.
-    
+
     Returns
     -------
     array doubles
         Redshift bin centers.
     """
-     # generate t_birth at the middle of each self.delta_t bin
-    t_birth_bin = [cosmology.age(0.).value]
+    # generate t_birth at the middle of each self.delta_t bin
+    t_birth_bin = [cosmology.age(0.0).value]
     t_birth = []
-    n_redshift_bin_centers = int(cosmology.age(0).to('Myr').value/delta_t)
-    for i in range(n_redshift_bin_centers+1):
-        t_birth.append(t_birth_bin[i] - delta_t*1e-3/2.) # Gyr
-        t_birth_bin.append(t_birth_bin[i] - delta_t*1e-3) # Gyr
+    n_redshift_bin_centers = int(cosmology.age(0).to("Myr").value / delta_t)
+    for i in range(n_redshift_bin_centers + 1):
+        t_birth.append(t_birth_bin[i] - delta_t * 1e-3 / 2.0)  # Gyr
+        t_birth_bin.append(t_birth_bin[i] - delta_t * 1e-3)  # Gyr
     t_birth = np.array(t_birth)
     # compute the redshift
     z_birth = []
-    for i in range(n_redshift_bin_centers+1):
+    for i in range(n_redshift_bin_centers + 1):
         # z_at_value is from astopy.cosmology
         z_birth.append(z_at_value(cosmology.age, t_birth[i] * u.Gyr))
     z_birth = np.array(z_birth)
