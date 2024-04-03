18dc18f5e8ef7392c005a0ebc1fc8162d098beba
Eirini <kasdaglie@ufl.edu>
2024-04-03 16:16:07 -0400

    add the initial params in the tracebacks

--- posydon/utils/posydonerror.py
+++ posydon/utils/posydonerror.py
@@ -54,3 +54,21 @@ class MatchingError(POSYDONError):
 
 class IntegrationError(POSYDONError):
     """Integration errors"""
+
+def initial_condition_message(binary,ini_params = None ):
+    if ini_params is None:
+        ini_params = ["\n Failed Binary Initial Conditions:\n",
+                    f"S1 mass: {binary.star_1.mass_history[0]} \n",
+                    f"S2 mass: {binary.star_2.mass_history[0]} \n",
+                    f"S1 state: {binary.star_1.state_history[0]} \n",
+                    f"S2 state: { binary.star_2.state_history[0]}\n",
+                    f"orbital period: { binary.orbital_period_history[0] } \n",
+                    f"eccentricity: { binary.eccentricity_history[0]} \n",
+                    f"binary state: { binary.state_history[0] }\n",
+                    f"binary event: { binary.state_history[0] }\n",
+                    f"S1 natal kick array: { binary.star_1.natal_kick_array }\n",
+                    f"S2 natal kick array: { binary.star_2.natal_kick_array}"]
+    message = ""
+    for i in ini_params:
+        message +=  i 
+    return message
