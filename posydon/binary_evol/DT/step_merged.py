425a212011f83699effbd12e97da24ec466d5240
ezapartas <ezapartas@gmail.com>
2024-05-16 16:33:22 +0300

    import copy...

--- posydon/binary_evol/DT/step_merged.py
+++ posydon/binary_evol/DT/step_merged.py
@@ -14,6 +14,7 @@ from posydon.utils.data_download import PATH_TO_POSYDON_DATA
 from posydon.binary_evol.singlestar import STARPROPERTIES, convert_star_to_massless_remnant
 from posydon.utils.common_functions import check_state_of_star
 from posydon.binary_evol.DT.step_isolated import IsolatedStep
+import copy
 
 import warnings
 
