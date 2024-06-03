8664602bbb60dff2ba8ff778598bee18050e5869
Eirini <kasdaglie@ufl.edu>
2024-06-03 13:24:23 -0400

    changing the save file name of the outputs

--- posydon/spectral_synthesis/default_options.py
+++ posydon/spectral_synthesis/default_options.py
@@ -11,18 +11,17 @@ default_grid_kwargs = {
     # Default grid options:
 
     # The main grid option:
-    'main_grid':'sg-C3K-coarse.h5', #"sg-CAP18-coarse.h5",
-
+    'main_grid':'sg-C3K-v1-solar.h5',#'sg-C3K-coarse.h5',"sg-CAP18-coarse.h5",
     # Secondary grid option to capture the failures of the main grid.
     'secondary_grid': "sg-BSTAR2006-ufl-vtb2.h5",
     'stripped_grid': "sg-Gotberg23.h5",
-    'bstar_grid' : "sg-BSTAR2006-ufl-vtb2.h5",#,'sg-C3K-coarse.h5'
+    'bstar_grid' :'sg-C3K-coarse.h5',# "sg-BSTAR2006-ufl-vtb2.h5",#,'sg-C3K-coarse.h5'
     'ostar_grid': "sg-OSTAR2002-ufl.h5",
     'WR_grid' : "sg-PoWR-WNL-H20.h5",
 
     # Setting the wavelengths range:
-    'lam_min': 105.,
-    'lam_max': 50000.,
+    'lam_min': 110.,
+    'lam_max': 27581.,
 
     # Number of points in the wavelength range:
     'lam_res': 300000,
