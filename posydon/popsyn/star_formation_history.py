fix rate calculation if select_one_met is not given to take the full SFR

posydon/popsyn/star_formation_history.py
@@ -274,8 +274,8 @@ def SFR_Z_fraction_at_given_redshift(
         Z_max_mask = Z <= Z_max
         redshift_indices = np.array([np.where(redshifts <= i)[0][0] for i in z])
         Z_dist = M[:, Z_max_mask][redshift_indices]
-
         fSFR = np.zeros((len(z), len(metallicity_bins) - 1))
+        
         for i in range(len(z)):
             if Z_dist[i].sum() == 0.0:
                 continue
@@ -291,8 +291,13 @@ def SFR_Z_fraction_at_given_redshift(
 
                 if not select_one_met:
                     # add the fraction of the SFR in the first and last bin
-                    fSFR[i, 0] = Z_dist_cdf_interp(np.log10(metallicity_bins[1]))
-                    fSFR[i, -1] = 1 - Z_dist_cdf_interp(np.log10(metallicity_bins[-1]))
+                    # or the only bin without selecting one metallicity
+                    if len(metallicity_bins) == 2:
+                        fSFR[i, 0] = 1
+                    else:
+                        
+                        fSFR[i, 0] = Z_dist_cdf_interp(np.log10(metallicity_bins[1]))
+                        fSFR[i, -1] = 1 - Z_dist_cdf_interp(np.log10(metallicity_bins[-1]))
     else:
         raise ValueError("Invalid SFR!")
 

posydon/popsyn/synthetic_population.py
@@ -1959,7 +1959,7 @@ class TransientPopulation(Population):
         indices = self.indices
 
         # sample the SFH for only the events that are within the Hubble time
-        # I only need to sample the SFH at each metallicity and z_birth
+        # only need to sample the SFH at each metallicity and z_birth
         # Not for every event!
         SFR_at_z_birth = star_formation_rate(rates.MODEL["SFR"], z_birth)
         # get metallicity bin edges
@@ -2026,7 +2026,7 @@ class TransientPopulation(Population):
                 .flatten()
                 * Zsun
             )
-
+            
             weights = np.zeros((len(met_events), nr_of_birth_bins))
             for i, met in enumerate(rates.centers_metallicity_bins):
                 mask = met_events == met
@@ -2668,7 +2668,10 @@ class Rates(TransientPopulation):
             bin_met[1:-1] = met_val[:-1] + (met_val[1:] - met_val[:-1]) / 2.0
         # one metallicty bin
         elif len(met_val) == 1:
-            if isinstance(self.MODEL["dlogZ"], float):
+            if self.MODEL["dlogZ"] is None:
+                bin_met[0] = -9
+                bin_met[-1] = 0
+            elif isinstance(self.MODEL["dlogZ"], float):
                 bin_met[0] = met_val[0] - self.MODEL["dlogZ"] / 2.0
                 bin_met[-1] = met_val[0] + self.MODEL["dlogZ"] / 2.0
             elif isinstance(self.MODEL["dlogZ"], list) or isinstance(