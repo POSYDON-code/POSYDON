c1a87dd7be4699c598347ca03b69700509868749
Dimitris Souropanis <qmh9626@quser31.quest.it.northwestern.edu>
2024-08-29 05:44:16 -0500

    synthetic

--- posydon/popsyn/synthetic_population.py
+++ posydon/popsyn/synthetic_population.py
@@ -992,16 +992,38 @@ class Population(PopulationIO):
                 Pwarn(f"{filename} already contains a mass_per_metallicity "
                       "table. Overwriting the table!", "OverwriteWarning")
 
-            simulated_mass = np.sum(self.oneline[["S1_mass_i", "S2_mass_i"]].to_numpy())
+           try:
+        # Filter the data based on the condition
+                filtered_data = self.oneline[self.oneline["state_i"] == "initially_single_star"]
+                filtered_data1 = self.oneline[self.oneline["state_i"] != "initially_single_star"]
+    
+        # Calculate the simulated masses
+                simulated_mass_binaries = np.sum(filtered_data1[["S1_mass_i", "S2_mass_i"]].to_numpy())
+                simulated_mass_single = np.sum(filtered_data[["S1_mass_i", "S2_mass_i"]].to_numpy())
+                simulated_mass = np.sum(self.oneline[["S1_mass_i", "S2_mass_i"]].to_numpy())
+    
+            except TypeError as e:
+                print(f"KeyError: {e}. Check if the specified columns exist in the DataFrame.")
+                
+                filtered_data = None
+                filtered_data1 = self.oneline[(self.oneline["state_i"] != "initially_single_star")]
+                simulated_mass_binaries = np.sum(filtered_data1[["S1_mass_i", "S2_mass_i"]].to_numpy())
+                simulated_mass_single = None
+                simulated_mass = np.sum(self.oneline[["S1_mass_i", "S2_mass_i"]].to_numpy())
+                print(1)
+            
             underlying_mass = initial_total_underlying_mass(
-                df=simulated_mass, **self.ini_params
-            )[0]
+            df=simulated_mass, df1=simulated_mass_single, df2= simulated_mass_binaries, **self.ini_params
+                )[0]
+  
+            
+                #df=simulated_mass, **self.ini_params)[0]
             self.mass_per_metallicity = pd.DataFrame(
-                index=[metallicity],
-                data={
-                    "simulated_mass": simulated_mass,
-                    "underlying_mass": underlying_mass,
-                    "number_of_systems": len(self.oneline),
+                    index=[metallicity],
+                    data={
+                        "simulated_mass_total": simulated_mass,
+                        "underlying_mass": underlying_mass,
+                        "number_of_systems": len(self.oneline),
                 },
             )
 
