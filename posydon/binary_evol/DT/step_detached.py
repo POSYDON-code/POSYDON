c66cd9dfc0fda6d7cf856890122c7213f78019c7
Camille <cliotine@u.northwestern.edu>
2024-10-08 13:10:50 -0500

    move accreted_He state to list of HMS star states

--- posydon/binary_evol/DT/step_detached.py
+++ posydon/binary_evol/DT/step_detached.py
@@ -40,7 +40,7 @@ import posydon.utils.constants as const
 from posydon.utils.posydonerror import NumericalError, MatchingError, POSYDONError
 from posydon.utils.posydonwarning import Pwarn
 
-LIST_ACCEPTABLE_STATES_FOR_HMS = ["H-rich_Core_H_burning"]
+LIST_ACCEPTABLE_STATES_FOR_HMS = ["H-rich_Core_H_burning", "accreted_He_Core_H_burning"]
 
 LIST_ACCEPTABLE_STATES_FOR_postMS = [
     "H-rich_Shell_H_burning",
@@ -48,8 +48,7 @@ LIST_ACCEPTABLE_STATES_FOR_postMS = [
     "H-rich_Central_He_depleted",
     "H-rich_Core_C_burning",
     "H-rich_Central_C_depletion",
-    "H-rich_non_burning",
-    "accreted_He_Core_H_burning"]
+    "H-rich_non_burning"]
 
 LIST_ACCEPTABLE_STATES_FOR_HeStar = [
     'stripped_He_Core_He_burning',
