e9a04784f60ac609b3878ef7e085b1acb6574985
Elizabeth Teng <eteng@protonmail.com>
2025-02-21 14:26:21 -0600

    fix issues with function calls

--- posydon/unit_tests/popsyn/test_rate_calculation.py
+++ posydon/unit_tests/popsyn/test_rate_calculation.py
@@ -15,6 +15,7 @@ sp = totest.sp
 # module you like to test
 from pytest import fixture, raises, warns, approx
 from inspect import isroutine, isclass
+from scipy.interpolate import CubicSpline
 
 # define test classes collecting several test functions
 class TestElements:
@@ -68,15 +69,15 @@ class TestFunctions:
     def test_get_shell_comoving_volume(self):
         # 2 missing arguments
         with raises(TypeError, match="missing 2 required positional arguments: 'z_hor_i' and 'z_hor_f'"):
-            totest.test_get_shell_comoving_volume()
+            totest.get_shell_comoving_volume()
         # 1 missing argument
         with raises(TypeError, match="missing 1 required positional argument: 'z_hor_f'"):
-            totest.test_get_shell_comoving_volume(0.1)
+            totest.get_shell_comoving_volume(0.1)
         # examples
         tests = [(0.1, 1.0, approx(97.7972132977263, abs=6e-12)),\
                  (0.3, 2.0, approx(277.8780499884267, abs=6e-12))]
         for (z1, z2, v) in tests:
-            assert totest.test_get_shell_comoving_volume(z1, z2) == v
+            assert totest.get_shell_comoving_volume(z1, z2) == v
             
     def test_get_comoving_distance_from_redshift(self):
         # missing argument
@@ -91,12 +92,12 @@ class TestFunctions:
     def test_get_cosmic_time_from_redshift(self):
         # missing argument
         with raises(TypeError, match="missing 1 required positional argument: 'z'"):
-            totest.test_get_cosmic_time_from_redshift()
+            totest.get_cosmic_time_from_redshift()
         # examples
         tests = [(0.1, approx(12.453793290949799, abs=6e-12)),\
                  (1.0, approx(5.862549255024051, abs=6e-12))]
         for (z, t) in tests:
-            assert totest.test_get_cosmic_time_from_redshift(z) == t
+            assert totest.get_cosmic_time_from_redshift(z) == t
         
     def test_redshift_from_cosmic_time_interpolator(self):
         interp = totest.redshift_from_cosmic_time_interpolator()
@@ -105,30 +106,30 @@ class TestFunctions:
     def test_get_redshift_from_cosmic_time(self):
         # missing argument
         with raises(TypeError, match="missing 1 required positional argument: 't_cosm'"):
-            totest.test_get_redshift_from_cosmic_time()
+            totest.get_redshift_from_cosmic_time()
         # examples
         tests = [(0.1, approx(29.8325299, abs=6e-12)),\
                  (1.0, approx(5.67584779, abs=6e-12))]
         for (t, z) in tests:
-            assert totest.test_get_cosmic_time_from_redshift(t) == z        
+            assert totest.get_cosmic_time_from_redshift(t) == z        
         
     def test_get_redshift_bin_edges(self):
         # missing argument
         with raises(TypeError, match="missing 1 required positional argument: 'delta_t'"):
-            totest.test_get_redshift_bin_edges()
+            totest.get_redshift_bin_edges()
         # examples
         tests = [(100., approx(0.006963184181145605, abs=6e-12)),\
                  (1000., approx(0.07301543666184201, abs=6e-12))]
         for (t,arr) in tests:
-            assert totest.test_get_redshift_bin_edges(t)[1] == arr
+            assert totest.get_redshift_bin_edges(t)[1] == arr
         
     def test_get_redshift_bin_centers(self):
         # missing argument
         with raises(TypeError, match="missing 1 required positional argument: 'delta_t'"):
-            totest.test_get_redshift_bin_centers()
+            totest.get_redshift_bin_centers()
         # examples
         tests = [(100., approx(49.33542627789386, abs=6e-12)),\
                  (1000., approx(13.957133275502315, abs=6e-12))]
         for (t,arr) in tests:
-            assert totest.test_get_redshift_bin_centers(t)[-1] == arr
+            assert totest.get_redshift_bin_centers(t)[-1] == arr
         
