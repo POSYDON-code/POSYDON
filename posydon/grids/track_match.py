fe5ba6d3aa64dfcc3fbd7f9695f98c8c4fc8174d
sgossage <sethg45@gmail.com>
2025-05-21 19:47:45 -0500

    really remove star2 argument from get_star_data

--- posydon/grids/track_match.py
+++ posydon/grids/track_match.py
@@ -864,7 +864,7 @@ class track_matcher:
         return initials[0], initials[1], htrack
     
 
-    def get_star_data(self, binary, star1, star2, copy_prev_m0=None, copy_prev_t0=None): #, co, copy_prev_m0=None, copy_prev_t0=None):
+    def get_star_data(self, binary, star1, copy_prev_m0=None, copy_prev_t0=None):
                 """Get and interpolate the properties of stars.
 
                 The data of a compact object can be stored as a copy of its
