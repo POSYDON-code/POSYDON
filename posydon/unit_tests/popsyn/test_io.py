cc24c65eb7ef3f948e56d4ca382552b2527d8a57
Elizabeth Teng <eteng@protonmail.com>
2025-07-02 15:49:54 -0500

    add tests for binarypop_kwargs_from_ini and simprop_kwargs_from_ini

--- posydon/unit_tests/popsyn/test_io.py
+++ posydon/unit_tests/popsyn/test_io.py
@@ -15,7 +15,9 @@ pd = totest.pd
 # module you like to test
 from pytest import fixture, raises, warns, approx
 from inspect import isroutine, isclass
+import importlib
 import textwrap
+import ast
 import os
 import errno
 import pprint
@@ -77,6 +79,66 @@ class TestFunctions:
         with open(file_path, "w") as f:
             f.write("test")
         return file_path
+    
+    @fixture
+    def sim_ini(self,tmp_path):
+        ini_content = """
+        [flow]
+        import = ['posydon.binary_evol.flow_chart', 'flow_chart']
+        absolute_import = None
+
+        [step_HMS_HMS]
+        import = ['posydon.binary_evol.MESA.step_mesa', 'MS_MS_step']
+        absolute_import = None
+        interpolation_method = 'linear3c_kNN'
+        save_initial_conditions = True
+        verbose = False
+
+        [extra_hooks]
+        import_1 = ['posydon.binary_evol.simulationproperties', 'TimingHooks']
+        absolute_import_1 = None
+        kwargs_1 = {}
+        import_2 = ['posydon.binary_evol.simulationproperties', 'StepNamesHooks']
+        absolute_import_2 = None
+        kwargs_2 = {}
+        """
+        file_path = os.path.join(tmp_path, "sim.ini")
+        with open(file_path, "w") as f:
+            f.write(ini_content)
+        return file_path
+    
+    @fixture
+    def binpop_ini(self, tmp_path):
+        ini_content = """
+        [BinaryPopulation_options]
+        use_MPI = False
+        metallicity = [0.02]
+        number_of_binaries = 1
+        temp_directory = 'tmp'
+
+        [BinaryStar_output]
+        extra_columns = {}
+        only_select_columns = []
+        scalar_names = []
+
+        [SingleStar_1_output]
+        include_S1 = False
+
+        [SingleStar_2_output]
+        include_S2 = False
+
+        [flow]
+        import = ['builtins', 'int']
+
+        [extra_hooks]
+        import_1 = ['builtins', 'int']
+        absolute_import_1 = None
+        kwargs_1 = {}
+        """
+        file_path = os.path.join(tmp_path, "binpop.ini")
+        with open(file_path, "w") as f:
+            f.write(ini_content)
+        return file_path
 
     
 #     def test_clean_binary_history_df(self):
@@ -111,11 +173,56 @@ class TestFunctions:
         assert parser.has_option("section", "key2")
 
         
-#     def test_simprop_kwargs_from_ini(self):
-#         pass
+    def test_simprop_kwargs_from_ini(self,monkeypatch,sim_ini):
+        # example
+        dummy_cls = type('DummyClass', (), {})()
+
+        # Patch importlib.import_module to return dummy modules with dummy classes
+        def dummy_import_module(name, package=None):
+            class DummyModule:
+                pass
+            setattr(DummyModule, 'TimingHooks', dummy_cls)
+            setattr(DummyModule, 'StepNamesHooks', dummy_cls)
+            setattr(DummyModule, 'flow_chart', dummy_cls)
+            setattr(DummyModule, 'MS_MS_step', dummy_cls)
+            return DummyModule()
+
+        monkeypatch.setattr(importlib, "import_module", dummy_import_module)
+
+        simkwargs = totest.simprop_kwargs_from_ini(sim_ini)
+
+        # Check keys exist
+        assert 'flow' in simkwargs
+        assert 'step_HMS_HMS' in simkwargs
+        assert 'extra_hooks' in simkwargs
+
+        # Check classes mapped to dummy_cls
+        assert simkwargs['flow'][0] is dummy_cls
+        assert simkwargs['step_HMS_HMS'][0] is dummy_cls
+
+        # extra_hooks is a list of tuples (class, kwargs)
+        hooks = simkwargs['extra_hooks']
+        assert isinstance(hooks, list)
+        assert hooks[0][0] is dummy_cls
+        assert hooks[0][1] == {}
+        assert hooks[1][0] is dummy_cls
+        assert hooks[1][1] == {}
         
-#     def test_binarypop_kwargs_from_ini(self):
-#         pass
+    def test_binarypop_kwargs_from_ini(self,monkeypatch,binpop_ini):
+        # example
+        class DummySimProps:
+            def __init__(self, **kwargs):
+                self.config = kwargs
+
+        monkeypatch.setattr(totest, "SimulationProperties", DummySimProps)
+
+        binkwargs = totest.binarypop_kwargs_from_ini(binpop_ini)
+
+        assert isinstance(binkwargs, dict)
+        assert binkwargs["metallicity"] == [0.02]
+        assert isinstance(binkwargs["population_properties"], DummySimProps)
+        assert "flow" in binkwargs["population_properties"].config
+
         
     def test_create_run_script_text(self):
         # missing argument
