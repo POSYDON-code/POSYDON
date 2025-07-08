73968b938684e90a2223995ffc369c001db09318
Matthias <122798003+mkruckow@users.noreply.github.com>
2025-07-08 21:07:27 +0200

    No test of max_number_of_runs (#617)
    * Update psygrid.py
    
    no cover of  max_number_of_runs
    
    * Update test_psygrid.py
    
    remove test of max_number_of_runs
    
    * fix unintended removal

--- posydon/grids/psygrid.py
+++ posydon/grids/psygrid.py
@@ -593,7 +593,7 @@ class PSyGrid:
 
         # Determine expected number of runs (may not be as many in the end)
         N_runs = len(grid.runs)
-        if not self.config['max_number_of_runs'] is None:
+        if not self.config['max_number_of_runs'] is None: # pragma: no cover
             N_runs = min(N_runs, self.config['max_number_of_runs'])
 
         # Decide on the columns to be included
@@ -684,8 +684,8 @@ class PSyGrid:
             self._say('Processing {}'.format(run.path))
 
             # Restrict number of runs if limit is set inside config
-            if self.config['max_number_of_runs'] is not None:
-                if run_index == self.config['max_number_of_runs']: # pragma: no cover
+            if self.config['max_number_of_runs'] is not None: # pragma: no cover
+                if run_index == self.config['max_number_of_runs']:
                     self._say("Maximum number of runs reached.")
                     break
 
