f29152444c2a6cf7f37ba6d371cb514180d9333e
Elizabeth Teng <41969755+elizabethteng@users.noreply.github.com>
2025-11-07 16:29:18 -0600

    avoid non-iterable loops for coverage
    Refactor value mapping logic to handle both 1D and 2D values more efficiently.

--- posydon/utils/interpolators.py
+++ posydon/utils/interpolators.py
@@ -224,12 +224,9 @@ class SingleStarInterpolator:
             # Map values to keys
             keys = self._keys[(is_positive, is_derivative)]
 
-            if np.ndim(values) == 1:
-                for i, key in enumerate(keys):
-                    results[key] = values[i]
-            else:
-                if len(keys)>0:
-                    for i, key in enumerate(keys):
-                        results[key] = values[:, i]
+            values = np.atleast_2d(values)
+            
+            for i, key in enumerate(keys):
+                results[key] = values[:, i] if values.shape[0] > 1 else values[0, i]
 
         return results
