47a0d37f1c05d6854bdfcd8084fe9892407125cc
Elizabeth Teng <41969755+elizabethteng@users.noreply.github.com>
2025-12-18 08:20:43 -0600

    outline script

---
+++ dev-tools/validate_binaries.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# A script for validating the outputs of 100 binaries, 
+# which can be compared to a baseline to monitor changes to the code.
+
+# script usage: ./validate_binaries.sh --branch candidate-branch
+
+# run candidate binaries and save to file
+# ./evolve_binaries.sh
+# save to file
+
+# evaluate tolerance for quantitative values
+
+# evaluate qualitative differences
+
+# warnings and error tracking
+
+# structured output
+
+# compare
+# python compare_runs.py baseline.json candidate.json
+
+
+
+
+
+# outputs/baseline.h5
+# outputs/candidate_branchname.h5
+# outputs/comparison_branchname.txt
