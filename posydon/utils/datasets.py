119a52d5a7369950dfed662619eaef52b42fb696
Seth Gossage <sethg45@gmail.com>
2025-12-21 02:20:17 -0600

    update md5 checksum

--- posydon/utils/datasets.py
+++ posydon/utils/datasets.py
@@ -192,7 +192,7 @@ ZENODO_COLLECTION['2025_school_data'] = {
                    + "from the school. Populations ranging from 10 to "\
                    + "1 million binaries are included, along with some small "\
                    + "MESA grid data for one of the labs.",
-    'md5': "b5ee19b546a3377101efe61738902b70",
+    'md5': "c675fbcae43962b99bbbd8599cb21eff",
     'title': "POSYDON School 2025 population and MESA data",
     'url': "https://zenodo.org/records/17902460"
 }
