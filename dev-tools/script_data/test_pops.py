36e0f23184a30e94f957c37ba8b59b76e281063d
Seth Gossage <sethg45@gmail.com>
2026-02-24 00:39:05 -0600

    move batches directory creation to save method in BinaryPopulation so it only does that when trying to save a pop. Updating scripts to evolve a population and print some of the binaries, fix entropy

--- dev-tools/script_data/test_pops.py
+++ dev-tools/script_data/test_pops.py
@@ -2,23 +2,46 @@ import os
 import shutil
 import tracemalloc
 
+from tabulate import tabulate
+import pandas as pd
+from binaries_suite import write_binary_to_screen
+
 from posydon.config import PATH_TO_POSYDON
 from posydon.popsyn.binarypopulation import BinaryPopulation
 from posydon.popsyn.synthetic_population import PopulationRunner
 
 
+def print_pop_settings(population):
+
+    print("Population settings:")
+
+    ignore_kwargs = ["extra_columns", "only_select_columns", "scalar_names", 
+                     "include_S1", "S1_kwargs", "include_S2", "S2_kwargs",
+                     "population_properties", "warnings_verbose", "history_verbose",
+                      "error_checking_verbose", "use_MPI", "read_samples_from_file", 
+                      "RANK", "size", "optimize_ram", "ram_per_cpu", 
+                      "dump_rate", "tqdm", "temp_directory", "breakdown_to_df"]
+    
+    for key, val in population.kwargs.items():
+        if key in ignore_kwargs:
+            continue
+        else:
+            print(f"\t {key} : {val}")
+
 def test_popruns():
 
-    #path_to_default_params = os.path.join(PATH_TO_POSYDON, "posydon/popsyn/population_params_default.ini")
-    my_ini_filename = "population_params.ini"
+    path_to_default_params = os.path.join(PATH_TO_POSYDON, "dev-tools/script_data/test_population_params.ini")
 
-    # Copy the defult .ini file into the current directory with new .ini filename defined above
-    #my_ini_path = shutil.copyfile(path_to_default_params, my_ini_filename)
+    pop = BinaryPopulation.from_ini(path_to_default_params, verbose=False)
+    print("Evolving BinaryPopulation...\n")
 
-    pop = BinaryPopulation.from_ini(my_ini_filename, verbose=True)
-    print("Evolving BinaryPopulation...")
+    print_pop_settings(pop)
+    
+    pop.evolve(breakdown_to_df=False, optimize_ram=False, tqdm=False)
 
-    pop.evolve(optimize_ram=False, tqdm=False)
+    #print(pop.manager.binaries)
+    write_binary_to_screen(pop.manager.binaries[0])
+
     print("Done!")
 
     #del pop
