4cc5ce01e8a4b2c8c4254013aac88bc9e4c0a796
Seth Gossage <sethg45@gmail.com>
2026-04-27 15:00:05 -0500

    update the other instance of hard coded m2[0] value

--- posydon/unit_tests/popsyn/test_independent_sample.py
+++ posydon/unit_tests/popsyn/test_independent_sample.py
@@ -74,7 +74,7 @@ class TestFunctions:
         assert orb_p[0] == approx(872.213878458193,abs=6e-12)
         assert ecc_p[0] == approx(0.7259611833901314,abs=6e-12)
         assert m1_p[0] == approx(19.97764511120556,abs=6e-12)
-        assert m2_p[0] == approx(9.328252086070083,abs=6e-12)
+        assert m2_p[0] == approx(8.964150262412895,abs=6e-12)
         assert isinstance(orb_p, np.ndarray)
         assert len(orb_p) == 1
         assert all(np.isfinite(m1_p))
