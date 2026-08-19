9e4adbe464ecf629cad261bc2cbc9da66c3e5d2e
Seth Gossage <sethg45@gmail.com>
2026-08-19 15:06:29 -0500

    add note in doc string about multi-metallicity behavior

--- posydon/binary_evol/simulationproperties.py
+++ posydon/binary_evol/simulationproperties.py
@@ -578,6 +578,10 @@ class SimulationProperties:
         limits the number of simultaneously stored TrackMatcher objects and
         reduces memory usage.
 
+        Note that in multi-metallicity runs, each metallicity is assigned to a 
+        unique BinaryPopulation, each of which has its own unique 
+        SimulationProperties at that given metallicity..
+
         Parameters
         ----------
         metallicity : float
